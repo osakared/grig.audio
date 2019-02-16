72565b65d9521125e4826cc4e1cb8fe3d6ea2cdc
Thomas J. Webb <thomas@osakared.io>
2019-02-16 12:46:18 -0800

    Necessary fix for dox generation

--- src/grig/audio/AudioInterface.hx
+++ src/grig/audio/AudioInterface.hx
@@ -1,5 +1,7 @@
 package grig.audio;
 
+import tink.core.Future;
+
 #if heaps // we need to override if making a plugin or if the user forces override
 typedef AudioInterface = NativeChannelAudioInterface;
 #elseif (js && !nodejs)
