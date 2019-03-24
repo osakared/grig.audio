acebef9c1db8293a0407bf7fabc310ce10ce28c0
Thomas J. Webb <thomas@osakared.io>
2019-03-24 10:58:16 -0700

    Removed windows on travis and added release to gitlab

--- release_haxelib.sh
+++ release_haxelib.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 rm -f library.zip
 zip -r library.zip src *.md *.json *.hxml .haxerc
-haxelib submit library.zip $HAXELIB_PWD --always
+haxelib submit library.zip $1 --always
 rm library.zip
